b4db729b56639ba9918b3a9fcec89dc93998b9dc
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-10-23 19:36:45 +0200

    Add additional country parameters and css style

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // links to html
-const url = 'https://restcountries.eu/rest/v1/name/';
+const url = 'https://restcountries.eu/rest/v2/name/';
+
 const countriesList = document.querySelector('#countries');
 const searchBtn = document.querySelector('#search');
 const countryInput = document.querySelector('#country-name');
+
+searchCountries();
 
 // event listeners
 searchBtn.addEventListener('click', searchCountries);
@@ -27,8 +30,27 @@ function searchCountries() {
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item){
-        let liEl = document.createElement('li');
-        liEl.innerText = item.name;
+        let liEl = document.createElement('div');
+        liEl.innerHTML = 
+        `<div class="country-heading">
+            <img src=${item.flag} alt="">
+            <h3>${item.name}</h3>
+        </div>
+        <ul class="properties">
+            <li>Native name:</li>
+            <li>Language:</li>
+            <li>Area:</li>
+            <li>Capital:</li>
+            <li>Currency:</li>
+        </ul>
+        <ul class="values">
+            <li>${item.nativeName}</li>
+            <li>${item.languages[0].name}</li>
+            <li>${item.area}</li>
+            <li>${item.capital}</li>
+            <li>${item.currencies[0].name}</li>
+        </ul>`
+        //item.name+item.area;
         countriesList.appendChild(liEl);
     });
 }
